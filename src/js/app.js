d87ffb5d9e02f8ae9f069cd8b1783436141ab34f
Petr Brovkin <bropet@mail.ru>
2020-05-22 19:36:22 +0300

    final

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+// TODO: write your code here
+import Character from './classes/Character';
+import Bowman from './classes/Bowman';
+import Swordsman from './classes/Swordsman';
+import Magician from './classes/Magician';
+import Undead from './classes/Undead';
+import Zombie from './classes/Zombie';
+import Daemon from './classes/Daemon';
+import Team from './classes/Team';
+import ErrorRepository from './classes/ErrorRepository';
+import Settings from './classes/Settings'; // настройки игры
